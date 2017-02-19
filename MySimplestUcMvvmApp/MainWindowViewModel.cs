3b030631cdeb98ad67ff4275cb0d880938444d20
Uwe on APOS-NBW10-02 <uhw@gmx.org>
2017-02-19 20:33:13 +0100

    Start.

---
+++ MySimplestUcMvvmApp/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimplestUcMvvmApp
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private String stringFromMainWindowViewModelToMainWindowView;
+        public String StringFromMainWindowViewModelToMainWindowView
+        {
+            get { return stringFromMainWindowViewModelToMainWindowView; }
+            set
+            {
+                if (value != StringFromMainWindowViewModelToMainWindowView)
+                {
+                    stringFromMainWindowViewModelToMainWindowView = value;
+                    OnPropertyChanged("StringFromMainWindowViewModelToUserControlDP");
+                }
+            }
+        }
+
+        private String stringFromMainWindowViewModelToUserControlsDP;
+        public String StringFromMainWindowViewModelToUserControlDP
+        {
+            get { return stringFromMainWindowViewModelToUserControlsDP; }
+            set
+            {
+                if (value != StringFromMainWindowViewModelToUserControlDP)
+                {
+                    stringFromMainWindowViewModelToUserControlsDP = value;
+                    OnPropertyChanged("StringFromMainWindowViewModelToUserControlDP");
+                }
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            StringFromMainWindowViewModelToMainWindowView = "String From MainWindow ViewModel To MainWindow View";
+            StringFromMainWindowViewModelToUserControlDP = "String From MainWindow ViewModel To UserControl DP";
+        }
+
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+}
---
+++ MySimplestUcMvvmApp/MyUserControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySimplestUcMvvmApp
+{
+    /// <summary>
+    /// Interaktionslogik für MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private String stringFromUserControl;
+        public String StringFromUserControl
+        {
+            get { return stringFromUserControl; }
+            set
+            {
+                if (value != StringFromUserControl)
+                {
+                    stringFromUserControl = value;
+                    OnPropertyChanged("StringFromMainWindowViewModelToUserControlDP");
+                }
+            }
+        }
+
+        public String StringDPForUserControlToSetFromOutside
+        {
+            get { return (String)GetValue(StringDPForUserControlToSetFromOutsideProperty); }
+            set { SetValue(StringDPForUserControlToSetFromOutsideProperty, value); }
+        }
+        public static readonly DependencyProperty StringDPForUserControlToSetFromOutsideProperty =
+            DependencyProperty.Register("StringDPForUserControlToSetFromOutside", typeof(string), typeof(MyUserControl), new PropertyMetadata(""));
+
+        public MyUserControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            StringFromUserControl = "String From UserControl";
+        }
+
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+}
